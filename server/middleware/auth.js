06d5a894b07499014d11a3234f5084bafabb1102
Rajesh Kumar Choudhary <128174090+rajeshChy79@users.noreply.github.com>
2025-09-17 00:30:34 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,11 +5,11 @@ import User from '../models/User.js';
 export const protect = async (req, res, next) => {
   try {
     let token;
-    console.log(token);
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
     }
+    console.log("token",token);
 
     if (!token) {
       return res.status(401).json({
